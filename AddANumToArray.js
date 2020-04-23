53ae49534693f0fd51a42837157a0e215efcdf49
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2020-04-22 19:35:48 -0700

    Update AddANumToArray.js

--- AddANumToArray.js
+++ AddANumToArray.js
@@ -1,7 +1,6 @@
 // Google
 function addANumberToArray(a) {
-      let carry = 0;
-      let aNum = Number(a.join(''));
+      let aNum = Number(a.join('')) + 1;
       let result = Array.from(aNum.toString()).map(Number);
 return result;
 }
